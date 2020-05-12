8dc2e994598f4048d20222ad3485cd3a2eeab418
Fábio Costa <fabiodanielmc@gmail.com>
2020-05-12 15:39:16 +0200

    Printer Errors

--- spec/Code_challenges_spec.js
+++ spec/Code_challenges_spec.js
@@ -12,6 +12,18 @@ describe("Roman numerals decoder", ()=>{
         expect(solution("IV")).toEqual(4);
         expect(solution("MMVIII")).toEqual(2008);
         expect(solution("MDCLXVI")).toEqual(1666);
-        expect(solution("XIV")).toEqual(14)
+        expect(solution("XIV")).toEqual(14);
+    })
+})
+
+describe("Printer Errors", ()=>{
+    it("Should be a function", ()=>{
+        expect(typeof printerError).toEqual("function")
+    })
+    it("Should return the number of printing errors", ()=>{
+        let string = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+        expect(printerError(string)).toEqual("3/56");
+        expect(printerError("aaabbbbhaijjjm")).toEqual("0/14");
+        expect(printerError("aaaxbbbbyyhwawiwjjjwwm")).toEqual("8/22");
     })
 })
--- src/Code_challenges.js
+++ src/Code_challenges.js
@@ -1,4 +1,4 @@
-// Roman Numerals Decoder
+// *Roman Numerals Decoder
 //Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
 // You don't need to validate the form of the Roman numeral.
 //  Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, 
@@ -65,3 +65,31 @@ function solution(roman){
     }
     return sum
 }
+
+//*Printer Errors
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity,
+// are named with letters from a to m.
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm 
+//meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced
+// e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+// You have to write a function printer_error which given a string will output the error rate of the printer as 
+//a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. 
+//Don't reduce this fraction to a simpler expression.
+// The string has a length greater or equal to one and contains only letters from ato z.
+// #Examples:
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+
+function printerError(str){
+    let a = 0;
+    for(let i = 0; i< str.length; i ++){
+        if(str[i] > "m"){
+            a++
+        }
+    }
+    return `${a}/${str.length}`
+}
+
