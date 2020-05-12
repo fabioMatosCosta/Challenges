dfb90be093dcf381973c797b355d3b5ff521153f
Fábio Costa <fabiodanielmc@gmail.com>
2020-05-12 16:42:38 +0200

    Find the divisors

--- spec/Code_challenges_spec.js
+++ spec/Code_challenges_spec.js
@@ -34,9 +34,25 @@ describe("Your order, please", ()=>{
     })
     it("Should order the words in a string according to the number in the word", ()=>{
         expect(orderString("is2 Thi1s T4est 3a")).toEqual("Thi1s is2 3a T4est");
-        expect(orderString("4of Fo1r pe6ople g3ood th5e the2")).toEqual("Fo1r the2 g3ood 4of th5e pe6ople")
-        expect(orderString("")).toEqual("")
-        expect(orderString("1 4 3 5 2")).toEqual("1 2 3 4 5")
+        expect(orderString("4of Fo1r pe6ople g3ood th5e the2")).toEqual("Fo1r the2 g3ood 4of th5e pe6ople");
+        expect(orderString("")).toEqual("");
+        expect(orderString("1 4 3 5 2")).toEqual("1 2 3 4 5");
     })
 })
+
+describe("Find the divisors", ()=>{
+    it("Should be a function", ()=>{
+        expect(typeof divisors).toEqual("function");
+    })
+    it("Should return an array if the number is not prime", ()=>{
+        expect(typeof(divisors(15))).toEqual("object");
+        expect(divisors(15)).toEqual([3,5]);
+        expect(divisors(12)).toEqual([2, 3, 4, 6]);
+    })
+    it("Should return a string when the number is prime", ()=>{
+        expect(typeof(divisors(13))).toEqual("string");
+        expect(divisors(13)).toEqual("13 is prime");
+        expect(divisors(7)).toEqual("7 is prime");
+    })
+});
 
